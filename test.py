da7a6c903a6dba22d6e2ba64c4d25d175f2b625f
hawk-aj <54933414+hawk-aj@users.noreply.github.com>
2021-02-28 22:57:26 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,4 @@
+# a python code to be taken to when a qr code is scanned 
+# nothing serious just a bored guy trying out a weird ideA
+
+print("i could have found better use of my time but this was more fun")
